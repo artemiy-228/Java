1222e7434b85ac76ac6cca8d4125ae3607d7c179
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-03-17 02:35:50 +0500

    Update Vector3.java

--- Vector/Vector3.java
+++ Vector/Vector3.java
@@ -1,6 +1,8 @@
 import static java.lang.Math.*;
 
+/*This class represents Math Vector in 3-dimensional space. This class also has relation aggregation with other class Point, which represent Point in 3-dimensional space*/
 public class Vector3 {
+    //A vector is defined by two points. beginning and end
     private final Point3 startPoint;
     private final Point3 endPoint;
 
@@ -12,6 +14,7 @@ public class Vector3 {
         return endPoint;
     }
 
+    //First type of constructor by 2 points
     public Vector3(Point3 start, Point3 end) throws Exception {
         this.startPoint = start;
         this.endPoint = end;
@@ -19,6 +22,8 @@ public class Vector3 {
             throw new Exception("Null vector");
         }
     }
+
+    //Second type of constructor by 6 coords. first 3 cords is coords of start point, rest cords of end point
     public Vector3(double x1, double y1, double z1, double x2, double y2, double z2) throws Exception {
         this.startPoint = new Point3(x1, y1, z1);
         this.endPoint = new Point3(x2, y2, z2);
@@ -27,6 +32,7 @@ public class Vector3 {
         }
     }
 
+    //This method used to find module of vector.
     public double Module(){
         double valueX = pow(startPoint.getX() - endPoint.getX(), 2);
         double valueY = pow(startPoint.getY() - endPoint.getY(), 2);
@@ -34,6 +40,7 @@ public class Vector3 {
         return sqrt(valueX + valueY + valueZ);
     }
 
+    //Because Java doesn't have overrading of operators, i have created this method as static. 
     public static Vector3 Addition(Vector3 one, Vector3 two) throws Exception {
         double newX = one.endPoint.getX() - one.startPoint.getX() + two.endPoint.getX() - two.startPoint.getX();
         double newY = one.endPoint.getY() - one.startPoint.getY() + two.endPoint.getY() - two.startPoint.getY();
@@ -43,6 +50,7 @@ public class Vector3 {
                 one.startPoint.getX() + newX, one.startPoint.getY() + newY, one.startPoint.getZ() + newZ);
     }
 
+       //Because Java doesn't have overrading of operators, i have created this method as static. 
     public static Vector3 Subtraction(Vector3 one, Vector3 two) throws Exception {
         double newX = one.endPoint.getX() - one.startPoint.getX() - (two.endPoint.getX() - two.startPoint.getX());
         double newY = one.endPoint.getY() - one.startPoint.getY() - (two.endPoint.getY() - two.startPoint.getY());
@@ -53,6 +61,7 @@ public class Vector3 {
     }
 
 
+    
     @Override
     public String toString() {
         return String.format("Vector from point{%f, %f, %f} to point{%f, %f, %f} with length %f",
@@ -61,6 +70,7 @@ public class Vector3 {
     }
 
 
+    //Because Java doesn't have overrading of operators, i have created this method as static. 
     public static double ScalarMultiply(Vector3 one, Vector3 two){
         double oneValueX = one.endPoint.getX() - one.startPoint.getX();
         double oneValueY = one.endPoint.getY() - one.startPoint.getY();
@@ -72,6 +82,7 @@ public class Vector3 {
     }
 
 
+    //Because Java doesn't have overrading of operators, i have created this method as static. 
     public static double Cos(Vector3 one, Vector3 two){
         return ScalarMultiply(one, two) / (one.Module() * two.Module());
     }
