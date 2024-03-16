30544a6d1a60f3237b94bc13a9eb0b8be9c1eaee
Artemy <79500772+artemiy-228@users.noreply.github.com>
2024-03-17 02:22:46 +0500

    Text Analyzer

---
+++ TextAnalyzer/NegativeTextAnalyzer.java
@@ -0,0 +1,14 @@
+class NegativeTextAnalyzer extends KeywordAnalyzer {
+
+    private final String[] keywords = {":(", "=(", ":|"};
+
+    @Override
+    protected String[] getKeywords() {
+        return keywords;
+    }
+
+    @Override
+    protected Label getLabel() {
+        return Label.NEGATIVE_TEXT;
+    }
+}
